2639f2127177b7f0fb98123e9637073955305651
Nabil Benjaa <benn2613@dinf-0023-06b.DInf.fsci.usherbrooke.ca>
2019-12-03 16:56:19 -0500

    Logistic Regression Class refactoring

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -1,63 +1,10 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from sklearn.linear_model import LogisticRegression
-from data_utils.data_preprocessing import DataPreprocessing
-from metrics.metrics import Metrics
-from cross_validation.cross_validation import CrossValidation
-import numpy as np
-import pandas as pd
+from classifiers.abstract_classifier import AbstractClassifier
 
-class LogisticRegressionClassifier:
+
+class LogisticRegressionClassifier(AbstractClassifier):
 
     def __init__(self, penalty='l1', C=1):
-        self.logreg = LogisticRegression(penalty=penalty, C=C)
-        self.metrics = Metrics()
-        self.X_train, self.Y_train, self.X_test, self.Y_test = DataPreprocessing().naive_preprocessing_data()
-        self.cv = None
-
-    def train(self):
-        self.logreg.fit(self.X_train, self.Y_train)
-
-    def predict(self, x):
-        self.train()
-        return self.logreg.predict(x)
-
-    def evaluate(self, training=True, metrics="accuracy"):
-        if training:
-            x, y = self.X_train, self.Y_train
-        else:
-            x, y = self.X_test, self.Y_test
-
-        if metrics == "accuracy":
-            self.metrics.accuracy(self.logreg, x, y, training)
-
-        elif metrics == "log_loss":
-            self.metrics.log_loss(self.logreg, x, y, training)
-
-        elif metrics == "confusion_matrix":
-            self.metrics.confusion_matrix(self.logreg, x, y, training)
-
-        elif metrics == "roc":
-            self.metrics.plot_roc(self.logreg, x, y)
-
-    def tunning_model(self, hyperparameters, kfold, metrics):
-        self.cv = CrossValidation(self.logreg, hyperparameters, kfold)
-        self.cv.fit_cross_validation(self.X_train, self.Y_train)
-        model_tunned = LogisticRegressionClassifier(self.cv.get_best_hyperparams()['penalty'], self.cv.get_best_hyperparams()['C'])
-        model_tunned.train()
-        print("** After cross validation **")
-        model_tunned.evaluate(training=True, metrics=metrics)
-        model_tunned.evaluate(training=False, metrics=metrics)
-
-    def print_combination(self):
-        params, mean = [self.cv.get_clf().cv_results_[key] for key in ['params', 'mean_test_score']]
-        pty = [p['penalty'] for p in params]
-        c = [p['C'] for p in params]
-        gridsearch = pd.DataFrame([pd.Series(x) for x in [pty,  np.round(c,  decimals=2), np.round(mean*100.0, decimals=2)]]).T
-        gridsearch.columns = ['Penalty', 'C', 'Accuracy']
-        print(gridsearch)
-
-    def cross_validate(self, hyperparameters, kfold):
-        cross_validation = CrossValidation(self.logreg, hyperparameters, kfold)
-        cross_validation.fit_cross_validation(self.X_train, self.Y_train)
-        print('model_after_fitting ', cross_validation.get_clf())
+        super().__init__(LogisticRegression(penalty=penalty, solver="liblinear", C=C))
