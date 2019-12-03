bbc7a4215b159eba33bc6856ca7a92f4c4e232fc
Nabil Benjaa <benn2613@dinf-0023-06b.DInf.fsci.usherbrooke.ca>
2019-12-03 17:00:09 -0500

    FC Class refactoring

--- classifiers/fully_connected.py
+++ classifiers/fully_connected.py
@@ -1,37 +1,19 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from data_utils.data_preprocessing import DataPreprocessing
-from metrics.metrics import Metrics
 from sklearn.neural_network import MLPClassifier
+from classifiers.abstract_classifier import AbstractClassifier
 
-class FullyConnectedClassifier:
 
-    def __init__(self):
-        self.fc = MLPClassifier()
-        self.metrics = Metrics()
-        self.X_train, self.Y_train, self.X_test, self.Y_test = DataPreprocessing().naive_preprocessing_data()
+class FullyConnectedClassifier(AbstractClassifier):
 
-    def train(self):
-        self.fc.fit(self.X_train, self.Y_train)
-
-    def predict(self, x):
-        self.train()
-        return self.fc.predict(x)
-
-    def evaluate(self, training=True, metrics="accuracy"):
-        if training:
-            x, y = self.X_train, self.Y_train
-        else:
-            x, y = self.X_test, self.Y_test
-
-        if metrics == "accuracy":
-            self.metrics.accuracy(self.fc, x, y, training)
-
-        elif metrics == "log_loss":
-            self.metrics.log_loss(self.fc, x, y, training)
-
-        elif metrics == "confusion_matrix":
-            self.metrics.confusion_matrix(self.fc, x, y, training)
-
-        elif metrics == "roc":
-            self.metrics.plot_roc(self.fc, x, y)
+    def __init__(self, hidden_layer_sizes=(10,), activation='relu', alpha=1e-2, learning_rate_init=1e-3, solver='adam'):
+        model = MLPClassifier(
+                random_state=None,
+                hidden_layer_sizes=hidden_layer_sizes,
+                activation=activation,
+                alpha=alpha,
+                learning_rate_init=learning_rate_init,
+                solver=solver,
+                max_iter=5000
+                )
+        super().__init__(model)
