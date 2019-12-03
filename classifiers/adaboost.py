90d0d5ec5cfaceb025cf5eaf6cfb03947ef4a121
Nabil Benjaa <benn2613@dinf-0023-06b.DInf.fsci.usherbrooke.ca>
2019-12-03 17:11:54 -0500

    Create Adaboost Classifier

--- classifiers/adaboost.py
+++ classifiers/adaboost.py
@@ -1,34 +1,10 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from sklearn.ensemble import AdaBoostClassifier
-from data_utils.data_preprocessing import DataPreprocessing
-from metrics.metrics import Metrics
+from classifiers.abstract_classifier import AbstractClassifier
 
-class AdaBoostAlgorithmClassifier:
+
+class AdaBoostAlgorithmClassifier(AbstractClassifier):
 
     def __init__(self):
-        self.model = AdaBoostClassifier(n_estimators=50, learning_rate=1)
-        self.metrics = Metrics()
-        self.X_train, self.Y_train, self.X_test, self.Y_test = DataPreprocessing().naive_preprocessing_data()
-
-    def train(self):
-        self.model.fit(self.X_train, self.Y_train)
-
-    def predict(self, x):
-        self.train()
-        return self.model.predict(x)
-
-    def evaluate(self, training=True, metrics="accuracy"):
-        if training:
-            x, y = self.X_train, self.Y_train
-        else:
-            x, y = self.X_test, self.Y_test
-
-        if metrics == "accuracy":
-            self.metrics.accuracy(self.model, x, y, training)
-
-        elif metrics == "confusion_matrix":
-            self.metrics.confusion_matrix(self.model, x, y, training)
-
-        elif metrics == "roc":
-            self.metrics.plot_roc(self.model, x, y)
+        super().__init__(AdaBoostClassifier(n_estimators=50, learning_rate=1))
